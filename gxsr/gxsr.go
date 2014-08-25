a86a879f0874e9118bb32248edd09f72be38c089
srimad <srimad@users.noreply.github.com>
2014-08-25 16:46:07 -0500

    open file

--- gxsr/gxsr.go
+++ gxsr/gxsr.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("")
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: " + os.Args[0] + " <file_path>")
+		return
+	}
+
+	inpfl, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	} 
+	defer inpfl.Close()
+
+	return
 }
