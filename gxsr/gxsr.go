02a8f9182499faca98528b6f191174b79608e7f2
srimad <srimad@users.noreply.github.com>
2014-08-25 19:01:46 -0500

    Before consolidated print

--- gxsr/gxsr.go
+++ gxsr/gxsr.go
@@ -3,7 +3,16 @@ package main
 import (
 	"fmt"
 	"os"
+	"bufio"
+	"strings"
+	"strconv"
 )
+
+func check_err(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -12,10 +21,43 @@ func main() {
 	}
 
 	inpfl, err := os.Open(os.Args[1])
-	if err != nil {
-		panic(err)
-	} 
+	check_err(err)
 	defer inpfl.Close()
+
+	type Vals struct {
+		count int
+		avg int64
+	}
+
+	val := Vals{count: 0, avg: 0}
+	data := make(map [string] Vals)
+	var eml string
+	var du int64
+	//var date string
+
+	scanner := bufio.NewScanner(inpfl)
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), ",")
+
+		eml = line[0]
+		du, _ = strconv.ParseInt(line[1], 10, 32)
+		//date = line[2]
+
+		if data[eml].count == 0 {
+			val.count = 1
+			val.avg = du 
+		} else {
+			val.count = data[eml].count + 1
+			val.avg = (data[eml].avg + du) / int64(2)
+		}
+		data[eml] = val
+		fmt.Println(eml)
+		fmt.Println(data[eml].count)
+		fmt.Println(data[eml].avg)
+	}
+	err = scanner.Err()
+	check_err(err)
+
 
 	return
 }
